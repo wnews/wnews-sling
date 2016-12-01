881607603dbe598ae46cdb5f7a34d7dc3cc82955
Ben Fortuna <benfortuna@users.noreply.github.com>
2016-12-01 16:03:05 +1100

    Added password override

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 : ${SLING_HOST?"Need to set SLING_HOST variable"}
 : ${SLING_PORT?"Need to set SLING_PORT variable"}
-./gradlew build bundleInstall -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT
+: ${SLING_PASSWORD?"Need to set SLING_PASSWORD variable"}
+./gradlew build bundleInstall -Psling_host=$SLING_HOST -Psling_port=$SLING_PORT -Psling_password=$SLING_PASSWORD
