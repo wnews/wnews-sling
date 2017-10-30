19cf88c49bc0942d904a14ceed066c771e2f7956
Ben Fortuna <bfortuna@odecee.com.au>
2017-10-31 10:45:19 +1100

    Remove dependency on whistlepost app

--- wnews-app/src/main/resources/SLING-INF/content/js/wnews.js
+++ wnews-app/src/main/resources/SLING-INF/content/js/wnews.js
@@ -1,3 +1,6 @@
+import 'jquery';
+import 'bootstrap';
 import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/grid.css';
 import '../css/style.css';
